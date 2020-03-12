7f19d4511879858a22c5e9ec3e3db4504128225c
Jordon Thompson <tho17088@byui.edu>
2020-03-11 20:23:49 -0600

    .

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ pool.query(sql, function(err, res) {
     console.log(`Error in query: ${err}`);
   }
 
-  console.log(`Back from DB with result: ${res.body}`);
+  console.log("Back from DB with result: " + res.body);
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
