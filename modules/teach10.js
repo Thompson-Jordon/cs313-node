c597b47b812c67f771aec61d271f9f935d5e8db0
Jordon Thompson <tho17088@byui.edu>
2020-03-12 20:49:30 -0600

    .

--- modules/teach10.js
+++ modules/teach10.js
@@ -4,11 +4,11 @@ const { Pool } = require("pg");
 const pool = new Pool({ connectionString: connectionString });
 
 exports.getPersons = (req, res) => {
-  personID = req.query.id;
+  let personID = req.query.id;
   console.log("Looking for id:", id);
 
   const sql =
-    "SELECT first_name, last_name, date_of_birth FROM persons WHERE id = $1";
+    'SELECT first_name, last_name, date_of_birth FROM persons WHERE id=$1::int';
   const values = [personID];
   pool.query(sql, values, function(err, result) {
     if (err) {
@@ -23,7 +23,7 @@ exports.getPersons = (req, res) => {
 };
 
 exports.getChildren = (req, response) => {
-  personID = req.query.id;
+  let personID = req.query.id;
   response.setHeader("Content-Type", "application/json");
 
   const sql = "SELECT child_id FROM children WHERE parent_id = $1";
@@ -41,7 +41,7 @@ exports.getChildren = (req, response) => {
 };
 
 exports.getParents = (req, response) => {
-  personID = req.query.id;
+  let personID = req.query.id;
   response.setHeader("Content-Type", "application/json");
 
   const sql = "SELECT parent_id FROM children WHERE child_id = $1";
